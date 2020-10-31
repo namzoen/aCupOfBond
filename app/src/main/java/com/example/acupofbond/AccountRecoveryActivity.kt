b420f9d3362f336cc0af2d7d683207a142c85c42
namzoen <taniarulinatalnael@gmail.com>
2020-10-31 13:21:40 +0700

    1. Intro slider is 95% done. 2. Sign In page is 98% done. 3. Sign Up page is 98% done. 4. Added new drawables for icons. 5. Other adjustments

---
+++ app/src/main/java/com/example/acupofbond/AccountRecoveryActivity.kt
@@ -0,0 +1,20 @@
+package com.example.acupofbond
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_account_recovery.*
+
+class AccountRecoveryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_recovery)
+
+        clickListeners()
+    }
+
+    private fun clickListeners() {
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/example/acupofbond/SignInActivity.kt
+++ app/src/main/java/com/example/acupofbond/SignInActivity.kt
@@ -1,11 +1,24 @@
 package com.example.acupofbond
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        clickListeners()
+    }
+
+    private fun clickListeners() {
+        linkSignUp.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        inTrouble.setOnClickListener {
+            startActivity(Intent(this, AccountRecoveryActivity::class.java))
+        }
     }
 }
