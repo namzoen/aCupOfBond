3c2b2995c0b2c9ece8762d8c317908bbd2c1026c
namzoen <bhoowakuls@gmail.com>
2021-01-08 20:27:17 +0700

    Chat bubbles added

--- app/src/main/java/com/example/acupofbond/ChatFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.acupofbond
-
-import androidx.fragment.app.Fragment
-
-class ChatFragment:Fragment(R.layout.fragment_chat) {
-}
---
+++ app/src/main/java/com/example/acupofbond/Message.kt
@@ -0,0 +1,6 @@
+package com.example.acupofbond
+
+
+data class Message(var user:String,
+                   var message:String,
+                   var time:Long)
