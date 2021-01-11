13ff2a8aa5a8e076c017a6541c7b4f1ff3bfbaae
namzoen <bhoowakuls@gmail.com>
2021-01-11 15:07:59 +0700

    Video call list added

--- app/src/main/java/com/example/acupofbond/VideoFragment.kt
+++ app/src/main/java/com/example/acupofbond/VideoFragment.kt
@@ -1,6 +1,17 @@
 package com.example.acupofbond
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class VideoFragment:Fragment(R.layout.fragment_video) {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view: View = inflater!!.inflate(R.layout.fragment_video, container, false)
+
+        return view
+    }
 }
