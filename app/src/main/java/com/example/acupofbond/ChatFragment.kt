036818d7030020ca96c841d4c3d5c9edb40fefb3
namzoen <bhoowakuls@gmail.com>
2021-01-11 13:48:44 +0700

    chatlist added (not functional)

--- app/src/main/java/com/example/acupofbond/ChatFragment.kt
+++ app/src/main/java/com/example/acupofbond/ChatFragment.kt
@@ -4,4 +4,11 @@ import androidx.fragment.app.Fragment
 
 class ChatFragment: Fragment(R.layout.fragment_chat) {
 
+//
+//    override fun onCreateView(
+//            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+//    ) {
+//
+//    }
+
 }
