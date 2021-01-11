c22427cef5808766330afc13a6404c5d31ccb1b6
namzoen <bhoowakuls@gmail.com>
2021-01-11 14:39:01 +0700

    Chat list added (not functional)

--- app/src/main/java/com/example/acupofbond/ChatFragment.kt
+++ app/src/main/java/com/example/acupofbond/ChatFragment.kt
@@ -1,14 +1,17 @@
 package com.example.acupofbond
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class ChatFragment: Fragment(R.layout.fragment_chat) {
-
-//
-//    override fun onCreateView(
-//            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-//    ) {
-//
-//    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view: View = inflater!!.inflate(R.layout.fragment_chat, container, false)
+        return view
+    }
 
 }
--- app/src/main/java/com/example/acupofbond/EditProfileActivity.kt
+++ app/src/main/java/com/example/acupofbond/EditProfileActivity.kt
@@ -1,35 +1,23 @@
 package com.example.acupofbond
 
 import android.os.Bundle
-import android.widget.RadioButton
-import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.chip.ChipGroup
 import kotlinx.android.synthetic.main.activity_editprofile.*
 
 
 class EditProfileActivity : AppCompatActivity() {
-    var radioGroup: RadioGroup? = null
-    lateinit var genderRadio: RadioButton
+    var chipGroup: ChipGroup? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editprofile)
 
-        radioGroup = findViewById(R.id.genderGroup)
-       // genderChoose(this)
+        chipGroup = findViewById(R.id.genderGroup)
+
 
         backBtn.setOnClickListener{
             finish()
         }
     }
-
-//    private fun genderChoose (v: EditProfileActivity){
-//        val selectedId = radioGroup!!.checkedRadioButtonId
-//        genderRadio = findViewById<View>(selectedId) as RadioButton
-//        if (selectedId == -1) {
-//            Toast.makeText(this, "Nothing selected", Toast.LENGTH_SHORT).show()
-//        } else {
-//            Toast.makeText(this, genderRadio.text, Toast.LENGTH_SHORT).show()
-//        }
-//    }
 }
