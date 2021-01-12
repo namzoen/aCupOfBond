de156cd1a8b8790a920a848890420a578c8fae7b
namzoen <bhoowakuls@gmail.com>
2021-01-12 10:19:36 +0700

    Home refined

--- app/src/main/java/com/example/acupofbond/EditProfileActivity.kt
+++ app/src/main/java/com/example/acupofbond/EditProfileActivity.kt
@@ -2,19 +2,14 @@ package com.example.acupofbond
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.chip.ChipGroup
 import kotlinx.android.synthetic.main.activity_editprofile.*
 
 
 class EditProfileActivity : AppCompatActivity() {
-    var chipGroup: ChipGroup? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editprofile)
-
-        chipGroup = findViewById(R.id.genderGroup)
-
 
         backBtn.setOnClickListener{
             finish()
